bb7d438cde1f71aa9e594ab901c7fd49eb301cf1
Duzduran <ahmetduzduran@gmail.com>
2023-01-10 14:15:05 +0300

    end point config added

--- src/main/java/com/SpringBootApp/Springbootapp/controller/HelloController.java
+++ src/main/java/com/SpringBootApp/Springbootapp/controller/HelloController.java
@@ -1,14 +1,17 @@
 package com.SpringBootApp.Springbootapp.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${message.welcome}")
+    public String welcomeMessage;
     @GetMapping("/")
     public String helloWorld(){
-        return "Welcome changedagainsdasdas";
+        return  welcomeMessage;
     }
 }
